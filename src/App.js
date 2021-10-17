58106231483551550103dde1c25e9be9af5fb339
Toan <toan.tranv1966@gmail.com>
2021-10-17 09:27:15 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import BaiTapState from './components/BaiTapState/BaiTapState';
+import ShoesStore from './components/BaiTapProps/ShoesStore';
+
+function App() {
+  return (
+    <div>
+      {/* <BaiTapState/> */}
+      <ShoesStore/>   
+      
+    </div>
+  );
+}
+
+export default App;
